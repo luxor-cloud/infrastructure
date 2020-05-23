1b0fb13921892116772cd1a5acbc7de092cc2d6e
Yannic Rieger <riegeryannic@gmail.com>
2020-05-23 23:06:00 +0200

    Proxy requests to nexus

--- terraform/vms.tf
+++ terraform/vms.tf
@@ -12,3 +12,15 @@ resource "hcloud_server" "nexus" {
     env = "prod"
   }
 }
+
+
+resource "hcloud_server" "general-nginx" {
+  name        = "general-nginx"
+  image       = "debian-10"
+  server_type = "cx11"
+  datacenter  = "nbg1-dc3"
+  ssh_keys    = ["${data.hcloud_ssh_key.ssh_key.id}"]
+  labels = {
+    env = "prod"
+  }
+}
