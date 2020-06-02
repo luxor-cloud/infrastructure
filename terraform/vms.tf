c7c0753416c75b2e6c013f8f38177a8df23e8875
Yannic Rieger <riegeryannic@gmail.com>
2020-06-02 21:49:09 +0200

    Add central postgres

--- terraform/vms.tf
+++ terraform/vms.tf
@@ -13,9 +13,19 @@ resource "hcloud_server" "nexus" {
   }
 }
 
-
 resource "hcloud_server" "general-nginx" {
   name        = "nginx.luxor.cloud"
+  image       = "debian-10"
+  server_type = "cx11"
+  datacenter  = "nbg1-dc3"
+  ssh_keys    = ["${data.hcloud_ssh_key.ssh_key.id}"]
+  labels = {
+    env = "prod"
+  }
+}
+
+resource "hcloud_server" "postgres" {
+  name        = "postgres.luxor.cloud"
   image       = "debian-10"
   server_type = "cx11"
   datacenter  = "nbg1-dc3"
